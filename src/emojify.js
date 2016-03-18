09f5f7100957cb0ea54af1f4513c16c23bcec5ac
Michael Rouse <mwrouse@outlook.com>
2016-03-18 14:36:38 -0500

    Update emojify.js

--- src/emojify.js
+++ src/emojify.js
@@ -1,4 +1,23 @@
-var emojis = { ':)': '😊', ':(': '😡', ':D': '😁', ';)': '😉', ':*': '😘', '*)': '😍', ':P': '😝', ';P': '😜', ":'(": '😢', 'X(': '😣', ':O': '😱', ':o': '😲', ':/': '😩', ':|': '😔' };
+var emojis = { 
+  ':)': '1F60A',
+  ':(': '1F621', 
+  ':D': '1F600', 
+  ":'D": '1F602',
+  ":')": '1F602',
+  ';)': '1F609',
+  ':*': '1F617',
+  '*)': '1F60D', 
+  ':P': '1F61B', 
+  ';P': '1F61C', 
+  ":'(": '1F622',
+  'X(': '1F623',
+  ':O': '1F631', 
+  ':o': '1F632',
+  ':/': '1F612',
+  ':\\': '1F615',
+  ':|': '1F611',
+  '': ''
+  };
 
 // Emojifies a string
 function emojify(str)
@@ -6,7 +25,7 @@ function emojify(str)
   // Replace all the emoticons
   for (var emoji in emojis)
   {
-    str = str.replace(new RegExp(emoji_escape(emoji), 'g'), emojis[emoji]);
+    str = str.replace(new RegExp(emoji_escape(emoji), 'g'), '&#x' + emojis[emoji] + ';');
   }
   
   return str;
